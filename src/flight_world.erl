f406ad2017550033188e7be24d4027ae06977669
Matthias <matthias@baktus.se>
2013-12-08 11:37:59 +0100

    add position functions to flight_world
    * is_valid_position/1
    * random_position/0

--- src/flight_world.erl
+++ src/flight_world.erl
@@ -4,7 +4,7 @@
 
 %% API
 -export([start_link/0]).
--export([valid_moves/2]).
+-export([valid_moves/2, is_valid_position/1, random_position/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -38,6 +38,14 @@ start_link() ->
 valid_moves(Position, Range) ->
   gen_server:call(?SERVER, {valid_moves, Position, Range}).
 
+-spec is_valid_position(position()) -> boolean().
+is_valid_position(Position) ->
+  gen_server:call(?SERVER, {is_valid_position, Position}).
+
+%% @doc Get a random position in the world, free from obstacles.
+-spec random_position() -> position().
+random_position() ->
+  gen_server:call(?SERVER, random_position).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -48,6 +56,12 @@ init([]) ->
 
 handle_call({valid_moves, Position, Range}, _From, State) ->
   Reply = get_valid_moves(State, Position, Range),
+  {reply, Reply, State};
+handle_call({is_valid_position, Position}, _From, State) ->
+  Reply = is_within_bounds(State, Position),
+  {reply, Reply, State};
+handle_call(random_position, _From, State) ->
+  Reply = get_random_position(State),
   {reply, Reply, State};
 handle_call(_Request, _From, State) ->
   Reply = ok,
@@ -81,6 +95,17 @@ get_valid_moves(State, Position, Range) ->
                   not lists:member(Pos, State#state.obstacles)
               end,
   lists:filter(FilterFun, InRange).
+
+%% @private
+%% @doc Get a random position in the world without obstacles.
+-spec get_random_position(state()) -> position().
+get_random_position(#state{ dimension_x = DimX,
+                            dimension_y = DimY,
+                            obstacles   = Obstacles }) ->
+  PossiblePositions = [ {X, Y} || X <- lists:seq(1, DimX),
+                                  Y <- lists:seq(1, DimY) ],
+  FreePositions = PossiblePositions -- Obstacles,
+  lists:nth(random:uniform(length(FreePositions)), FreePositions).
 
 %% @private
 %% @doc Get the positions in the world within range.
@@ -148,5 +173,34 @@ get_hypothetical_positions_test() ->
                 {2,1},
                 {2,2}],
                get_hypothetical_positions({1, 1}, 1)).
+
+is_within_bounds_test_() ->
+  State = #state{ dimension_x = 1,
+                  dimension_y = 1 },
+  [ ?_assertEqual(true,  is_within_bounds(State, {1, 1})),
+    ?_assertEqual(false, is_within_bounds(State, {0, 0})),
+    ?_assertEqual(false, is_within_bounds(State, {1, 0})),
+    ?_assertEqual(false, is_within_bounds(State, {2, 2})),
+    ?_assertEqual(false, is_within_bounds(State, {-1, 1}))
+  ].
+
+get_random_position_setup() ->
+  meck:new(random, [unstick, passthrough]),
+  meck:expect(random, uniform, fun(X) -> X end),
+  [random].
+
+get_random_position_cleanup(MeckedModules) ->
+  [meck:unload(Module) || Module <- MeckedModules].
+
+get_random_position_test_() ->
+  {setup, fun get_random_position_setup/0, fun get_random_position_cleanup/1,
+   [?_test(
+       begin
+         State = #state{ dimension_x = 2,
+                         dimension_y = 2,
+                         obstacles = [{1, 2}, {2, 2}] },
+         ?assertEqual({2, 1}, get_random_position(State))
+       end)
+    ]}.
 
 -endif.
